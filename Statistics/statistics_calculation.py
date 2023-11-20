62c0bac4edc3b842dd5dc22d29dc1ec507a9e182
sareh <sareh.aghaei-dinani@medunigraz.at>
2023-11-20 16:04:42 +0100

    apply cassis

--- Statistics/statistics_calculation.py
+++ Statistics/statistics_calculation.py
@@ -1,21 +1,38 @@
-import xml.etree.ElementTree as ET
+from cassis import *
 import os
 import pandas as pd
 import nltk
 nltk.download('punkt')
 from nltk.tokenize import word_tokenize
+import zipfile
 
-# layer_definition
-layer_concept_definition = './/webanno.custom.CustomMCN'
-layer_relation_definition = './/webanno.custom.Relation'
 
-def parse_xml_file(file_path):
-    tree = ET.parse(file_path)
-    root = tree.getroot()
-    return root
+def unzip_all(zip_folder, extract_to):
+    with zipfile.ZipFile(zip_folder, 'r') as zip_ref:
+        for file_info in zip_ref.infolist():
+            file_path_parts = file_info.filename.split('/')
+            extract_path = os.path.join(extract_to, *file_path_parts)
+            os.makedirs(os.path.dirname(extract_path), exist_ok=True)
+            with zip_ref.open(file_info) as file:
+                with open(extract_path, 'wb') as output_file:
+                    output_file.write(file.read())
+        print(f"Successfully extracted {zip_folder} to {extract_to}")
 
-def report(directory_path):
+def unzip_all_in_directory(directory_path):
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            if file.lower().endswith('.zip'):
+                zip_file_path = os.path.join(root, file)
+                extract_to = os.path.join(root, file.split('.')[0])
+                unzip_all(zip_file_path, extract_to)
+                # Recursively unzip files in the subfolder
+                unzip_all_in_directory(extract_to)
 
+
+
+def report(type_system_directory_path, unzip_directory, user_name, document_name):
+
+    xml_file = type_system_directory_path + 'TypeSystem.xml'
     # Initialize dictionaries to store concepts and pairs for all files
     concept_data = {}
     begin_end_pairs = []
@@ -23,42 +40,41 @@ def report(directory_path):
     relation_data  = {}
 
     relation_begin_end_pairs = []
-
+    with open(xml_file, 'rb') as f:
+        typesystem = load_typesystem(f)
     # Loop through all .xml files in the specified directory
-    for filename in os.listdir(directory_path):
-        if filename.endswith(".xml"):
-            file_path = os.path.join(directory_path, filename)
+    for root, dirs, files in os.walk(unzip_directory):
+        if user_name in root and document_name in root:
+            for file in files:
+                if file.lower().endswith('.xmi'):
+                    file_path = os.path.join(root, file)
+                    with open(file_path, 'rb') as fl:
+                        cas = load_cas_from_xmi(fl, typesystem=typesystem)
 
-            # Parse the XML file
-            root = parse_xml_file(file_path)
+                        # Iterate through the XML and extract concepts and begin-end pairs
+                        for surface in cas.select('webanno.custom.CustomMCN'):
 
-            # Iterate through the XML and extract concepts and begin-end pairs
-            for custom_mcn in root.findall(layer_concept_definition):
-                concept = custom_mcn.get('Concept')
-                begin = custom_mcn.get('begin')
-                end = custom_mcn.get('end')
+                            concept = surface.Concept
+                            begin = surface.begin
+                            end = surface.end
+                            if concept is not None:
+                                if concept not in concept_data:
+                                    concept_data[concept] = []
+                                concept_data[concept].append((begin, end))
+                            begin_end_pairs.append((begin, end))
 
-                if concept is not None:
-                    if concept not in concept_data:
-                        concept_data[concept] = set()
-                    concept_data[concept].add((begin, end))
+                        # Iterate through the XML and extract relations and begin-end pairs
+                        for surface in cas.select('webanno.custom.Relation'):
+                            relation = surface.Relation
+                            begin = surface.begin
+                            end = surface.end
 
-                begin_end_pairs.append((begin, end))
+                            if relation is not None:
+                                if relation not in relation_data:
+                                    relation_data[relation] = []#set()
+                                relation_data[relation].append((begin, end))#add((begin, end))
 
-
-            # Iterate through the XML and extract relations and begin-end pairs
-            for custom_mcn in root.findall(layer_relation_definition):
-                relation = custom_mcn.get('Relation')
-                begin = custom_mcn.get('begin')
-                end = custom_mcn.get('end')
-
-                if relation is not None:
-                    if relation not in relation_data:
-                        relation_data[relation] = set()
-                    relation_data[relation].add((begin, end))
-
-                relation_begin_end_pairs.append((begin, end))
-
+                            relation_begin_end_pairs.append((begin, end))
 
     concept_df = pd.DataFrame(concept_data.items(), columns=["Concept", "Begin-End Pairs"])
     concept_df["Number of Instances"] = concept_df["Begin-End Pairs"].apply(len)
@@ -71,15 +87,15 @@ def report(directory_path):
 
     # Count the distinct concepts and begin-end pairs for all files
     count_distinct_concepts = len(concept_df)
-    count_concept_instances = len(begin_end_pairs)
+    count_concept_instances = concept_df['Number of Instances'].sum()
 
     # Count the distinct relations and begin-end pairs for all files
     count_distinct_relations = len(relation_df)
-    count_relation_instances = len(relation_begin_end_pairs)
+    count_relation_instances = relation_df['Number of Instances'].sum()
 
     # Save the results to CSV files
-    concept_df.to_csv('concept_results.csv', index=False)
-    relation_df.to_csv('relation_results.csv', index=False)
+    concept_df.to_csv(os.path.join(unzip_directory + 'concept_results.csv'), index=False)
+    relation_df.to_csv(os.path.join(unzip_directory + 'relation_results.csv'), index=False)
 
     print(f'total number of distinct concepts: {count_distinct_concepts}')
     print(f'total number of instances of concepts: {count_concept_instances}')
@@ -87,6 +103,7 @@ def report(directory_path):
     print(f'total number of instances of relations: {count_relation_instances}')
 
     return concept_df, relation_df, count_distinct_concepts, count_concept_instances, count_distinct_relations, count_relation_instances
+
 
 def count_tokens_in_file(directory_path, language):
     num_tokens = 0
@@ -96,7 +113,7 @@ def count_tokens_in_file(directory_path, language):
             with open(os.path.join(directory_path, filename), 'r', encoding='utf-8') as file:
                 num_file += 1
                 text = file.read()
-                tokens = word_tokenize(text, language=language)  # Tokenize using the German language model
+                tokens = word_tokenize(text, language=language) # Tokenize using the German language model
                 num_tokens += len(tokens)
     if num_file == 0:
         return 0
@@ -105,30 +122,25 @@ def count_tokens_in_file(directory_path, language):
     print(f'average number of tokens within documents: {avg_count}')
     return avg_count
 
-
-
 if __name__ == "__main__":
 
-    # change values of xml_directory_path, doc_directory_path and doc_language
+    #export the whole project in Inception and put its path at export_directory
+    export_directory = '/home/sareh/Data/Inception/'
+    #define the directory path including type_file_system file
+    type_system_directory_path = '/home/sareh/Data/Inception/'
+    #unzip all the subfolders within the exported project
+    unzip_all_in_directory(export_directory)
 
-    # directory including the annotated documents (i.e., xml files)
-    xml_directory_path = '/home/sareh/Data/Inception/'
+    # calculate the number of distinct concepts and relations along with their instances
+    #specify the path for unzipping
+    unzip_directory = '/home/sareh/Data/Inception/'
+    report(type_system_directory_path, unzip_directory, 'heba', 'ST_BEHANDLUNGSBRIEF_Patien')#the other type of docs in mug is: ST_BEHANDLUNGSBRIEF_Patien
+
+
+    # count the average number of tokens within narratives:
     # directory including the input text documents
     doc_directory_path = '/home/sareh/Data/Inception/Documents for Annotation/ST_BEHANDLUNGSBRIEF/'  # PATHO_HISTOLOGIE/'
     # language of the input docs to count the number of the tokens
     doc_language = 'german'
-
-    # calculate the number of distinct concepts and relations along with their instances
-    report(xml_directory_path)
-    # count the average number of tokens within narratives
     count_tokens_in_file(doc_directory_path, doc_language)
-
-
-
-
-
-
-
-
-
 
