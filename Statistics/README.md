2d62d1bbea2b1df46e9da357d96af6240363b434
Kristian Kankainen <kristian@keeleleek.ee>
2023-11-24 18:41:44 +0200

    statistics are collected per user

--- Statistics/README.md
+++ Statistics/README.md
@@ -1,3 +1,13 @@
 # Statistics for AIDAVA annotation task
 
-To run the script, first select your project in Inception. Then choose settings -> Export -> Secondary format -> UIMA CAS XMI (XML 1.0).
+To download your annotation project, first select your project in Inception. Then choose settings -> Export -> Secondary format -> UIMA CAS XMI (XML 1.0).
+
+Save the exported annotation project(s) zip files into a folder named '"export-zips"' in the root folder, next to the script file.
+
+Run the script with the command 'python statistics_calculator.py'. At the first run it unzips the project files, consecutive executions calculate the statistics.
+
+
+Running the script takes some minutes at NEMC.
+
+
+Statistics are compiled per user. The csv files are saved reflecting the username under each projects unzipped folder in the 'export-zips' folder.
--- Statistics/statistics_calculation.py
+++ Statistics/statistics_calculation.py
@@ -16,7 +16,7 @@ def unzip_all(zip_folder, extract_to):
             with zip_ref.open(file_info) as file:
                 with open(extract_path, 'wb') as output_file:
                     output_file.write(file.read())
-        print(f"Successfully extracted {zip_folder} to {extract_to}")
+        #print(f"Successfully extracted {zip_folder} to {extract_to}")
 
 def unzip_all_in_directory(directory_path):
     for root, dirs, files in os.walk(directory_path):
@@ -28,79 +28,104 @@ def unzip_all_in_directory(directory_path):
                 # Recursively unzip files in the subfolder
                 unzip_all_in_directory(extract_to)
 
+def unzip_all_in_zip(zip_file_path):
+    if zip_file_path.lower().endswith('.zip'):
+        extract_to = zip_file_path[0:-4]
+        unzip_all(zip_file_path, extract_to)
+        # Recursively unzip files in the subfolder
+        unzip_all_in_directory(extract_to)
+        print("Unzipped a project. Run the script again to calculate its statistics.")
+    else:
+        print("Not a zip file!")
 
 
-def report(type_system_directory_path, unzip_directory, user_name, document_name):
-
-    xml_file = type_system_directory_path + 'TypeSystem.xml'
+# Compile a report for the exported project directory
+def report(project_folder):
     # Initialize dictionaries to store concepts and pairs for all files
-    concept_data = {}
-    begin_end_pairs = []
+    per_user_data = {}
 
-    relation_data  = {}
+    # Go through the UIMA xmi-file in the specified directory
+    annotation_xml_directory = os.path.join(project_folder, 'annotation')
+    if not os.path.isdir(annotation_xml_directory):
+        print("Skipping folder. Does not seem to be an Inception UIMA export: " + annotation_xml_directory)
+        return
 
-    relation_begin_end_pairs = []
-    with open(xml_file, 'rb') as f:
-        typesystem = load_typesystem(f)
-    # Loop through all .xml files in the specified directory
-    for root, dirs, files in os.walk(unzip_directory):
-        if user_name in root and document_name in root:
-            for file in files:
-                if file.lower().endswith('.xmi'):
-                    file_path = os.path.join(root, file)
-                    with open(file_path, 'rb') as fl:
-                        cas = load_cas_from_xmi(fl, typesystem=typesystem)
+    for annotated_file_directory in os.scandir(annotation_xml_directory):
+        if not annotated_file_directory.is_dir():
+            continue
+        # Collect statistics per user
+        for user in os.scandir(annotated_file_directory):
+            if not user.is_dir():
+                continue
+            if user.name in user_blacklist:
+                continue
+            if user.name not in per_user_data:
+                per_user_data[user.name] = {"concept_data":             {},
+                                            "begin_end_pairs":          [],
+                                            "relation_data":            {},
+                                            "relation_begin_end_pairs": []}
+            
+            file_path = os.path.join(user.path, user.name + ".xmi")
+            
+            xml_file = os.path.join(user.path, 'TypeSystem.xml')
+            with open(xml_file, 'rb') as fxml:
+                typesystem = load_typesystem(fxml)
+            with open(file_path, 'rb') as fxmi:
+                cas = load_cas_from_xmi(fxmi, typesystem = typesystem)
 
-                        # Iterate through the XML and extract concepts and begin-end pairs
-                        for surface in cas.select('webanno.custom.CustomMCN'):
+                # Iterate through the XML and extract concepts and begin-end pairs
+                for surface in cas.select('webanno.custom.CustomMCN'):
+                    concept = surface.Concept
+                    begin = surface.begin
+                    end = surface.end
+                    if concept is not None:
+                        if concept not in per_user_data[user.name]["concept_data"]:
+                            per_user_data[user.name]["concept_data"][concept] = []
+                        per_user_data[user.name]["concept_data"][concept].append((begin, end))
+                    per_user_data[user.name]["begin_end_pairs"].append((begin, end))
 
-                            concept = surface.Concept
-                            begin = surface.begin
-                            end = surface.end
-                            if concept is not None:
-                                if concept not in concept_data:
-                                    concept_data[concept] = []
-                                concept_data[concept].append((begin, end))
-                            begin_end_pairs.append((begin, end))
+                # Iterate through the XML and extract relations and begin-end pairs
+                for surface in cas.select('webanno.custom.Relation'):
+                    relation = surface.Relation
+                    begin = surface.begin
+                    end = surface.end
 
-                        # Iterate through the XML and extract relations and begin-end pairs
-                        for surface in cas.select('webanno.custom.Relation'):
-                            relation = surface.Relation
-                            begin = surface.begin
-                            end = surface.end
+                    if relation is not None:
+                        if relation not in per_user_data[user.name]["relation_data"]:
+                            per_user_data[user.name]["relation_data"][relation] = []#set()
+                        per_user_data[user.name]["relation_data"][relation].append((begin, end))#add((begin, end))
 
-                            if relation is not None:
-                                if relation not in relation_data:
-                                    relation_data[relation] = []#set()
-                                relation_data[relation].append((begin, end))#add((begin, end))
+                    per_user_data[user.name]["relation_begin_end_pairs"].append((begin, end))
+    
 
-                            relation_begin_end_pairs.append((begin, end))
+    # Print the collected statistics per user
+    for user in per_user_data:
+        concept_df = pd.DataFrame(per_user_data[user]["concept_data"].items(), columns=["Concept", "Begin-End Pairs"])
+        concept_df["Number of Instances"] = concept_df["Begin-End Pairs"].apply(len)
+        concept_df = concept_df.drop("Begin-End Pairs", axis=1)
 
-    concept_df = pd.DataFrame(concept_data.items(), columns=["Concept", "Begin-End Pairs"])
-    concept_df["Number of Instances"] = concept_df["Begin-End Pairs"].apply(len)
-    concept_df = concept_df.drop("Begin-End Pairs", axis=1)
+        # Create a single DataFrame for relations
+        relation_df = pd.DataFrame(per_user_data[user]["relation_data"].items(), columns=["Relation", "Begin-End Pairs"])
+        relation_df["Number of Instances"] = relation_df["Begin-End Pairs"].apply(len)
+        relation_df = relation_df.drop("Begin-End Pairs", axis=1)
 
-    # Create a single DataFrame for relations
-    relation_df = pd.DataFrame(relation_data.items(), columns=["Relation", "Begin-End Pairs"])
-    relation_df["Number of Instances"] = relation_df["Begin-End Pairs"].apply(len)
-    relation_df = relation_df.drop("Begin-End Pairs", axis=1)
+        # Count the distinct concepts and begin-end pairs for all files
+        count_distinct_concepts = len(concept_df)
+        count_concept_instances = concept_df['Number of Instances'].sum()
 
-    # Count the distinct concepts and begin-end pairs for all files
-    count_distinct_concepts = len(concept_df)
-    count_concept_instances = concept_df['Number of Instances'].sum()
+        # Count the distinct relations and begin-end pairs for all files
+        count_distinct_relations = len(relation_df)
+        count_relation_instances = relation_df['Number of Instances'].sum()
 
-    # Count the distinct relations and begin-end pairs for all files
-    count_distinct_relations = len(relation_df)
-    count_relation_instances = relation_df['Number of Instances'].sum()
+        # Save the results to CSV files
+        concept_df.to_csv(os.path.join(project_folder, 'concept_results-'+user+'.csv'), index=False)
+        relation_df.to_csv(os.path.join(project_folder, 'relation_results-'+user+'.csv'), index=False)
 
-    # Save the results to CSV files
-    concept_df.to_csv(os.path.join(unzip_directory + 'concept_results.csv'), index=False)
-    relation_df.to_csv(os.path.join(unzip_directory + 'relation_results.csv'), index=False)
-
-    print(f'total number of distinct concepts: {count_distinct_concepts}')
-    print(f'total number of instances of concepts: {count_concept_instances}')
-    print(f'total number of distinct relations: {count_distinct_relations}')
-    print(f'total number of instances of relations: {count_relation_instances}')
+        print(f'Analyzed for user: "' + user + '" file: ' + project_folder.name)
+        print(f'total number of distinct concepts: {count_distinct_concepts}')
+        print(f'total number of instances of concepts: {count_concept_instances}')
+        print(f'total number of distinct relations: {count_distinct_relations}')
+        print(f'total number of instances of relations: {count_relation_instances}')
 
     return concept_df, relation_df, count_distinct_concepts, count_concept_instances, count_distinct_relations, count_relation_instances
 
@@ -125,22 +150,32 @@ def count_tokens_in_file(directory_path, language):
 if __name__ == "__main__":
 
     #export the whole project in Inception and put its path at export_directory
-    export_directory = '/home/sareh/Data/Inception/'
-    #define the directory path including type_file_system file
-    type_system_directory_path = '/home/sareh/Data/Inception/'
-    #unzip all the subfolders within the exported project
-    unzip_all_in_directory(export_directory)
+    export_directory = './export-zips/'
 
-    # calculate the number of distinct concepts and relations along with their instances
-    #specify the path for unzipping
-    unzip_directory = '/home/sareh/Data/Inception/'
-    report(type_system_directory_path, unzip_directory, 'heba', 'ST_BEHANDLUNGSBRIEF_Patien')#the other type of docs in mug is: ST_BEHANDLUNGSBRIEF_Patien
+    if not os.path.exists(export_directory):
+        exit("Could not find the export folder ('"+export_directory+"'). Make sure it exists and run this script again.")
+    
+    user_blacklist = ['INITIAL_CAS', 'kristian', 'admin']
 
 
-    # count the average number of tokens within narratives:
-    # directory including the input text documents
-    doc_directory_path = '/home/sareh/Data/Inception/Documents for Annotation/ST_BEHANDLUNGSBRIEF/'  # PATHO_HISTOLOGIE/'
-    # language of the input docs to count the number of the tokens
-    doc_language = 'german'
-    count_tokens_in_file(doc_directory_path, doc_language)
+    with os.scandir(export_directory) as it:
+        for export_folder in it:
+            if export_folder.is_file() and export_folder.name.endswith('.zip'):
+                if os.path.isdir(export_folder.path[0:-4]):
+                    print("Not unzipping. Delete folder to unzip file again: " + export_folder.name)
+                else:
+                    unzip_all_in_zip(export_folder.path)
+                    print("Unzipped a new project. Run the script again to compile its statistics!")
+            if export_folder.is_dir():
+                print("Hold on, we're counting the statistics...")
+                report(export_folder)
+                #report(type_system_directory_path, unzip_directory, user_name, 'annotation')#the other type of docs in mug is: ST_BEHANDLUNGSBRIEF_Patien
+
+
+    ## count the average number of tokens within narratives:
+    ## directory including the input text documents
+    #doc_directory_path = '/home/sareh/Data/Inception/Documents for Annotation/ST_BEHANDLUNGSBRIEF/'  # PATHO_HISTOLOGIE/'
+    ## language of the input docs to count the number of the tokens
+    #doc_language = 'german'
+    #count_tokens_in_file(doc_directory_path, doc_language)
 
